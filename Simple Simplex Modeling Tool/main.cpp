4a90a34dcaaf8e8f242039c4f03e43e7ca932196
Ege Demirbas <30300963+Egerion@users.noreply.github.com>
2019-10-01 14:44:46 +0300

    Initial Commit

---
+++ Simple Simplex Modeling Tool/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Simple Simplex Modeling Tool
+//
+//  Created by Ege Demirbaş on 1.10.2019.
+//  Copyright © 2019 Ege Demirbaş. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
